80d15376cef06284ddadcf3aee1f8aeff49a7394
Denis_Guiraudet <guiraudet@live.fr>
2017-12-14 17:59:55 +0100

    JAVA : debut remplissage function

--- JAVA/ProjetEclipse/UML_Project/src/data/agence/Agence.java
+++ JAVA/ProjetEclipse/UML_Project/src/data/agence/Agence.java
@@ -4,8 +4,13 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 
+import data.immo.Appartement;
 import data.immo.BienImmo;
+import data.immo.Maison;
+import data.immo.Terrain;
 import data.user.Client;
+import data.user.Entreprise;
+import data.user.Personne;
 
 public class Agence {
 	
@@ -29,50 +34,72 @@ public class Agence {
 		
 	}
 	
-	public void creerClient(String nomV, String adresseV, int telephoneV, String emailV) {
+	public void creerPersonne(String nomV, String adresseV, String telephoneV, String emailV) {
 		
+		this.listeClient.add(new Personne(nomV, adresseV, telephoneV, emailV));
+
+	}
 		
+	public void creerEntreprise(String nomV, String adresseV, String telephoneV, String emailV,
+			String formeJuridiqueV, String numSirenV) {
+		
+		this.listeClient.add(new Entreprise(nomV, adresseV, telephoneV, emailV, formeJuridiqueV, numSirenV));
 
 	}
 	
 	public void priseRdvVendeur(Date dateV, Client clientV) {
 
-		
-		
-	}
-	
-	public void creerBien() {
-		
-		
-		
+		this.listeRdvVendeur.put(dateV, clientV);
 		
 	}
 	
-	public void creerMandat(Client clientV, Mandat mandatV) {
-		// TODO : pas mandatV ici ducon justement on le creer la
+	public void creerTerrain(int numV, String adresseV, String orientationV, Double prixV, Date venteV,
+			Double surfaceSolV, Double longueurFacadeV) {
 		
+		this.listeBien.add(new Terrain(numV, adresseV, orientationV, prixV, venteV, surfaceSolV, longueurFacadeV));
 		
+	}
+	
+	public void creerMaison(int numV, String adresseV, String orientationV, Double prixV, Date venteV,
+			Double surfaceHabitableV, int nbPiecesV, int nbEtagesV, String chauffageV) {
+		
+		this.listeBien.add(new Maison(numV, adresseV, orientationV, prixV, venteV, surfaceHabitableV, nbPiecesV, nbEtagesV, chauffageV));
+		
+	}
+	
+	public void creerAppartement(int numV, String adresseV, String orientationV, Double prixV, Date venteV,
+			int nbPiecesV, int etageV, Double chargesMensuellesV) {
+		
+		this.listeBien.add(new Appartement(numV, adresseV, orientationV, prixV, venteV, nbPiecesV, etageV, chargesMensuellesV));
+		
+	}
+	
+	public void creerMandat(Double prixV, Date venteV, Date debutV, Date finV,
+			BienImmo bienV, Client vendeurV) {
+		
+		this.listeMandat.add(new Mandat(prixV, venteV, debutV, finV, bienV, vendeurV));
 		
 	}
 	
 	public void organiserVisite(Client clientV, Mandat mandatV, Date dateV) {
 		
-		
+		mandatV.addRdvVisite(dateV, clientV);
 		
 	}
 	
 	public void organiserVente(Client clientV, Mandat mandatV, Date dateV) {
+		// TODO : LINK MANDAT TO listeRdvVendeur
 		
+		this.listeRdvVendeur.put(dateV, clientV);
 		
-		
-		//creerPromesse();
+		// TODO : creerPromesse();
 		
 	}
 	
-	private void creerPromesse(Promesse promesseV) {
-		// TODO : pas promesseV ici ducon justement on le creer la
+	private void creerPromesse(Double prixVerseVendeurV, String adresseNotaireV, Date venteV, Double commissionAgenceV, Double fraisVenteV,
+			BienImmo bienV, Client acheteurV, Notaire notaireV) {
 		
-		
+		this.listePromesse.add(new Promesse(prixVerseVendeurV, adresseNotaireV, venteV, commissionAgenceV, fraisVenteV, bienV, acheteurV, notaireV));
 		
 	}
 	
